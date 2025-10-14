d330a33fdb5eec3989db8daf44dd2ef16993f7b6
Andrew Hughes <andrewhughes86@gmail.com>
2025-10-13 21:57:34 -0600

    Added line numbering arg using the clap crate.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,36 @@
-use std::env;
-use std::fs;
+use clap::Parser;
+use std::{fs, path::PathBuf};
+
+/// Read a file and optionally add line numbers
+#[derive(Parser, Debug)]
+#[command(author = "Andy Hughes", version = "0.1", about = "A cat clone")]
+struct Args {
+    /// File to read
+    #[arg(value_name = "PATH")]
+    file_path: PathBuf,
+
+    /// number all output lines
+    #[arg(short = 'n', long = "number")]
+    number: bool,
+}
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    
-    let file_path = &args[1];
-    
-    let contents = fs::read_to_string(file_path)
-       .expect("Should have been able to read the file.");
- 
-    print!("{}", contents);
+    let args = Args::parse();
+
+    let contents = read_file(&args.file_path);
+    print_lines(&contents, args.number);
+}
+
+fn read_file(path: &PathBuf) -> String {
+    fs::read_to_string(path).unwrap_or_else(|_| panic!("Failed to read file {:?}", path))
+}
+
+fn print_lines(contents: &str, number: bool) {
+    for (i, line) in contents.lines().enumerate() {
+        if number {
+            println!("{:>6}\t{}", i + 1, line);
+        } else {
+            println!("{}", line);
+        }
+    }
 }
